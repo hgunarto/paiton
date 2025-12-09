433ef24d13cc03608f09f82cdf48841ef8bbb2ff
hgunarto <248272324+hgunarto@users.noreply.github.com>
2025-12-09 15:15:37 +0900

    Update home route response message

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python ini berjalan di Render!"
+    return "Python ini berjalan di Render, ditulis oleh H Gunarto!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
 
-print("Hello from Render!")
+# print("Hello from Render!")
